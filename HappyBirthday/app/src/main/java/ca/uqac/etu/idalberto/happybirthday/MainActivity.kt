f40af96b2f7936a676ebc8b68fd274ec5c9d5fb8
IDalb <synfoniz@gmail.com>
2024-09-05 13:42:17 -0400

    Ex2 - Section 4 Add Box layout and GreetingText (on top of the BG image)

--- HappyBirthday/app/src/main/java/ca/uqac/etu/idalberto/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/ca/uqac/etu/idalberto/happybirthday/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -28,7 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold { innerPadding ->
-                        GreetingText(
+                        GreetingImage(
                             message = "Happy Birthday Séra!",
                             from = "From Isaac",
                             modifier = Modifier.fillMaxSize().padding(innerPadding)
@@ -64,10 +65,17 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
     val image = painterResource(R.drawable.androidparty)
-    Image(
-        painter = image,
-        contentDescription = null
-    )
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier.fillMaxSize().padding(8.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
